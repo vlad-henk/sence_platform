8c345cac41335ab2178c7505d7317337923f1ccc
vlad-henk <vladsukhenko1539@gmail.com>
2023-04-18 11:47:52 +0300

    check if current_user is online

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,22 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    before_action :set_global_variables, if: :user_signed_in?   
-    
-    include Pundit::Authorization
-    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  
-    include PublicActivity::StoreController #save current_user using gem public_activity
-  
-    def set_global_variables
-      @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) #navbar search
-    end
+  after_action :user_activity
+  before_action :authenticate_user!
+  before_action :set_global_variables, if: :user_signed_in?
+
+  include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  include PublicActivity::StoreController #save current_user using gem public_activity
+
+  def set_global_variables
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) #navbar search
+  end
 
   private
+
+  def user_activity
+    current_user.try :touch
+  end
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
