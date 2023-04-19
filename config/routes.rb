2e77c38ef05ca32cc9cf4ed131068cc3c3ec3f39
vlad-henk <vladsukhenko1539@gmail.com>
2023-04-19 12:34:06 +0300

    nested resources. lessons in courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :lessons
   devise_for :users
-  resources :courses
+  resources :courses do
+    resources :lessons
+  end
   resources :users, only: [:index, :edit, :show, :update]
-  get 'home/index'
-  get 'home/activity'
   root 'home#index'
+  get 'home/activity'
+  get '/index', to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
