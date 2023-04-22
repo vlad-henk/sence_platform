be3a45dcdd34213a39fe8e882d75ca77c2c6129a
vlad-henk <vladsukhenko1539@gmail.com>
2023-04-22 21:36:37 +0300

    Track User progress in a Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,6 +5,7 @@ class Course < ApplicationRecord
   belongs_to :user, optional: true, counter_cache: true
   has_many :lessons, dependent: :destroy
   has_many :enrollments
+  has_many :user_lessons, through: :lessons
 
   validates :title, uniqueness: true
 
@@ -46,6 +47,12 @@ class Course < ApplicationRecord
 
   def bought(user)
     self.enrollments.where(user_id: [user.id], course_id: [self.id]).empty?
+  end
+
+  def progress(user)
+    unless self.lessons_count == 0
+      (user_lessons.where(user: user).count/self.lessons_count).to_f*100
+    end
   end
 
   def update_rating
