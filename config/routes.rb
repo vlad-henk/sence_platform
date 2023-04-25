d75f6c280d73473c0a6f708e98ef0c046df642e8
vlad-henk <vladsukhenko1539@gmail.com>
2023-04-25 20:07:10 +0300

    jquery-ui sortable - make lessons sortable and persist changes

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,9 @@ Rails.application.routes.draw do
       patch :approve
       patch :unapprove
     end
-    resources :lessons
+    resources :lessons do
+      put :sort
+    end
     resources :enrollments, only: [:new, :create]
   end
 
