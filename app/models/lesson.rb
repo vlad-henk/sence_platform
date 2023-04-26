e608e1997035f4fca166ad2f84fd4bc8b809d6b6
vlad-henk <vladsukhenko1539@gmail.com>
2023-04-26 17:13:23 +0300

    action_text restrict attachments, more validations for course and lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,7 @@ class Lesson < ApplicationRecord
   belongs_to :course, counter_cache: true
   has_many :user_lessons, dependent: :destroy
   validates :title, :content, :course, presence: true
+  validates :title, uniqueness: true, length: { maximum: 70 }
 
   has_rich_text :content
 
