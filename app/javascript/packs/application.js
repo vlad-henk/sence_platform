36532b05bd44c9923643ac26c872d2200eae6e35
vlad-henk <vladsukhenko1539@gmail.com>
2023-04-26 13:03:05 +0300

    action_text attachment restrictions, download and display

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,6 +19,7 @@ require("@rails/actiontext")
 
 import "chartkick/chart.js"
 
+import "../trix-editor-overrides"
 
 require("jquery") // yarn add jquery
 require("jquery-ui-dist/jquery-ui"); // yarn add jquery-ui-dist 
