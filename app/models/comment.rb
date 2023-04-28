2eaee889a6d7da58d513f46b10cf10d7934a2f41
vlad-henk <vladsukhenko1539@gmail.com>
2023-04-28 22:32:12 +0300

    gem public_activity for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,14 @@
 class Comment < ApplicationRecord
 
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
   belongs_to :user, counter_cache: true
   belongs_to :lesson, counter_cache: true
   #User.find_each { |user| User.reset_counters(user.id, :comments) }  
   #Lesson.find_each { |lesson| Lesson.reset_counters(lesson.id, :comments) }
 
+  def to_s
+    content
+  end
 end
