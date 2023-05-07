163124a00bfcf69f4b8dd5b31087df986e23b0a5
vlad-henk <vladsukhenko1539@gmail.com>
2023-05-07 12:47:58 +0300

    enrollment completion certificate and styling

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,5 +1,6 @@
 class EnrollmentsController < ApplicationController
-  before_action :set_enrollment, only: %i[ show edit update destroy ]
+  skip_before_action :authenticate_user!, :only => [:certificate]
+  before_action :set_enrollment, only: [:show, :edit, :update, :destroy, :certificate]
   before_action :set_coruse, only: [:new, :create]
 
   # GET /enrollments or /enrollments.json
@@ -15,6 +16,22 @@ class EnrollmentsController < ApplicationController
     @q = Enrollment.joins(:course).where(courses: {user: current_user}).ransack(params[:q])
     @pagy, @enrollments = pagy(@q.result.includes(:user))
     render 'index'
+  end
+
+  def certificate
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "#{@enrollment.course.title}, #{@enrollment.user.email}",
+        page_size: 'A4',
+        template: "enrollments/show.pdf.haml",
+        layout: "pdf.html.haml",
+        orientation: "Landscape",
+        lowquality: true,
+        zoom: 1,
+        dpi: 75
+      end
+    end
   end
 
   # GET /enrollments/1 or /enrollments/1.json
