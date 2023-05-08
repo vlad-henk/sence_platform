2b8aa7137f3f85e85c47037a9ef72f803ee3b87b
vlad-henk <vladsukhenko1539@gmail.com>
2023-05-08 20:57:47 +0300

    gem wicked - edit and update functionality

--- app/controllers/courses/course_wizard_controller.rb
+++ app/controllers/courses/course_wizard_controller.rb
@@ -1,22 +1,32 @@
 class Courses::CourseWizardController < ApplicationController
   include Wicked::Wizard
   before_action :set_progress, only: [:show]
+  before_action :set_course, only: [:show, :update, :finish_wizard_path]
 
   steps :basic_info, :details
 
   def show
-    @course = Course.friendly.find(params[:course_id])
-    #@user = current_user
-    #case step
-    #when :find_friends
-    #  @friends = @user.find_friends
-    #end
+    case step
+    when :basic_info
+    when :details
+      @tags = Tag.all
+    end
     render_wizard
+  end
+
+  def update
+    case step
+    when :basic_info
+      @course.update(course_params)
+    when :details
+      @tags = Tag.all
+      @course.update(course_params)
+    end
+    render_wizard @course
   end
 
   def finish_wizard_path
     # courses_path
-    @course = Course.friendly.find(params[:course_id])
     course_path(@course)
   end
 
@@ -28,4 +38,14 @@ class Courses::CourseWizardController < ApplicationController
         @progress = 0
       end
     end
+
+    def set_course
+      @course = Course.friendly.find(params[:course_id])
+    end
+
+    def course_params
+      params.require(:course).permit(:title, :description, :short_description, :price,
+        :published, :language, :level, :avatar, tag_ids: [])
+    end
+
 end
